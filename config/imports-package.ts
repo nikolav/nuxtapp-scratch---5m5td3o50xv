aa38c08d86b9f3ea8f652bbdd659de733b45d0b4
Vukovic Nikola <admin@nikolav.rs>
2024-11-10 22:26:27 +0100

    refactor import presets

---
+++ config/imports-package.ts
@@ -0,0 +1,268 @@
+export const IMPORTS = [
+  {
+    from: "lodash/get",
+    imports: [
+      {
+        name: "default",
+        as: "get",
+      },
+    ],
+  },
+  {
+    from: "lodash/get",
+    imports: [
+      {
+        name: "default",
+        as: "getPath",
+      },
+    ],
+  },
+  {
+    from: "lodash/identity",
+    imports: [{ name: "default", as: "identity" }],
+  },
+  {
+    from: "lodash/isFunction",
+    imports: [{ name: "default", as: "isFunction" }],
+  },
+  {
+    from: "lodash/range",
+    imports: [{ name: "default", as: "range" }],
+  },
+  {
+    from: "lodash/pullAll",
+    imports: [{ name: "default", as: "pullAll" }],
+  },
+  {
+    from: "lodash/size",
+    imports: [{ name: "default", as: "len" }],
+  },
+  {
+    from: "lodash/isEmpty",
+    imports: [{ name: "default", as: "isEmpty" }],
+  },
+  {
+    from: "lodash/first",
+    imports: [{ name: "default", as: "first" }],
+  },
+  {
+    from: "lodash/every",
+    imports: [{ name: "default", as: "every" }],
+  },
+  {
+    from: "lodash/differenceBy",
+    imports: [{ name: "default", as: "differenceBy" }],
+  },
+  {
+    from: "lodash/union",
+    imports: [{ name: "default", as: "union" }],
+  },
+  {
+    from: "lodash/sortBy",
+    imports: [{ name: "default", as: "sortBy" }],
+  },
+  {
+    from: "lodash/once",
+    imports: [{ name: "default", as: "once" }],
+  },
+  {
+    from: "lodash/difference",
+    imports: [{ name: "default", as: "difference" }],
+  },
+  {
+    from: "lodash/sampleSize",
+    imports: [{ name: "default", as: "sampleSize" }],
+  },
+  {
+    from: "lodash/findKey",
+    imports: [{ name: "default", as: "findKey" }],
+  },
+  {
+    from: "lodash/find",
+    imports: [{ name: "default", as: "find" }],
+  },
+  {
+    from: "lodash/unset",
+    imports: [{ name: "default", as: "unset" }],
+  },
+  {
+    from: "lodash/each",
+    imports: [{ name: "default", as: "each" }],
+  },
+  {
+    from: "lodash/cloneDeep",
+    imports: [{ name: "default", as: "cloneDeep" }],
+  },
+  // {
+  //   from: "lodash/clone",
+  //   imports: [{ name: "default", as: "clone" }],
+  // },
+  {
+    from: "lodash/isString",
+    imports: [{ name: "default", as: "isString" }],
+  },
+  {
+    from: "lodash/has",
+    imports: [{ name: "default", as: "has" }],
+  },
+  {
+    from: "lodash/has",
+    imports: [{ name: "default", as: "hasPath" }],
+  },
+  {
+    from: "lodash/pick",
+    imports: [{ name: "default", as: "pick" }],
+  },
+  {
+    from: "lodash/omit",
+    imports: [{ name: "default", as: "omit" }],
+  },
+  {
+    from: "lodash/keys",
+    imports: [{ name: "default", as: "keys" }],
+  },
+  // {
+  //   from: "lodash/isEqual",
+  //   imports: [{ name: "default", as: "isEqual" }],
+  // },
+  {
+    from: "lodash/sample",
+    imports: [{ name: "default", as: "sample" }],
+  },
+  {
+    from: "lodash/take",
+    imports: [{ name: "default", as: "take" }],
+  },
+  {
+    from: "lodash/takeRight",
+    imports: [{ name: "default", as: "takeRight" }],
+  },
+  {
+    from: "lodash/debounce",
+    imports: [{ name: "default", as: "debounce" }],
+  },
+  {
+    from: "lodash/startCase",
+    imports: [{ name: "default", as: "startCase" }],
+  },
+  {
+    from: "lodash/lowerCase",
+    imports: [{ name: "default", as: "lowerCase" }],
+  },
+  {
+    from: "lodash/upperCase",
+    imports: [{ name: "default", as: "upperCase" }],
+  },
+  {
+    from: "lodash/camelCase",
+    imports: [{ name: "default", as: "camelCase" }],
+  },
+  {
+    from: "lodash/kebabCase",
+    imports: [{ name: "default", as: "kebabCase" }],
+  },
+  {
+    from: "lodash/snakeCase",
+    imports: [{ name: "default", as: "snakeCase" }],
+  },
+  {
+    from: "lodash/set",
+    imports: [{ name: "default", as: "set" }],
+  },
+  {
+    from: "lodash/clamp",
+    imports: [{ name: "default", as: "clamp" }],
+  },
+  {
+    from: "lodash/uniqueId",
+    imports: [{ name: "default", as: "uniqueId" }],
+  },
+  {
+    from: "lodash/assign",
+    imports: [{ name: "default", as: "assign" }],
+  },
+  {
+    from: "lodash/some",
+    imports: [{ name: "default", as: "some" }],
+  },
+  {
+    from: "lodash/trimEnd",
+    imports: [{ name: "default", as: "trimEnd" }],
+  },
+  {
+    from: "lodash/trim",
+    imports: [{ name: "default", as: "trim" }],
+  },
+  {
+    from: "lodash/noop",
+    imports: [{ name: "default", as: "noop" }],
+  },
+  {
+    from: "lodash/transform",
+    imports: [{ name: "default", as: "transform" }],
+  },
+  {
+    from: "lodash/map",
+    imports: [{ name: "default", as: "map" }],
+  },
+  {
+    from: "lodash/filter",
+    imports: [{ name: "default", as: "filter" }],
+  },
+  {
+    from: "lodash/reduce",
+    imports: [{ name: "default", as: "reduce" }],
+  },
+  {
+    from: "lodash/escapeRegExp",
+    imports: [{ name: "default", as: "escapeRegExp" }],
+  },
+  {
+    from: "lodash/intersection",
+    imports: [{ name: "default", as: "intersection" }],
+  },
+  {
+    from: "lodash/intersectionBy",
+    imports: [{ name: "default", as: "intersectionBy" }],
+  },
+  {
+    from: "lodash/uniqBy",
+    imports: [{ name: "default", as: "uniqBy" }],
+  },
+  {
+    from: "lodash/uniq",
+    imports: [{ name: "default", as: "uniq" }],
+  },
+  {
+    from: "lodash/merge",
+    imports: [{ name: "default", as: "merge" }],
+  },
+  {
+    from: "lodash/values",
+    imports: [{ name: "default", as: "values" }],
+  },
+  {
+    from: "lodash/last",
+    imports: [{ name: "default", as: "last" }],
+  },
+  {
+    from: "validator/lib/isEmail",
+    imports: [{ name: "default", as: "isEmail" }],
+  },
+  {
+    from: "validator/lib/isMobilePhone",
+    imports: [{ name: "default", as: "isMobilePhone" }],
+  },
+  {
+    from: "validator/lib/isURL",
+    imports: [{ name: "default", as: "isURL" }],
+  },
+  {
+    from: "uuid",
+    imports: [{ name: "v4", as: "uuid" }],
+  },
+  {
+    from: "tree-model",
+    imports: [{ name: "default", as: "TreeModel" }],
+  },
+];
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ import vitePluginVuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 import { API_URL, SSR, BASE_DIR, ENDPOINT_GRAPHQL } from "./config";
 import { trimEndBase } from "./utils/trim-end-base";
 
+import { IMPORTS } from "./config/imports-package";
+
 type TMeta = Record<string, string>[];
 
 const meta: TMeta = [
@@ -25,7 +27,7 @@ export default defineNuxtConfig({
   // # client-side rendering;
   //  prerender    .true
   //  no-prerender .false
-  // ssr: SSR,
+  ssr: SSR,
 
   routeRules: {
     //   // Generated at build time for SEO purpose
@@ -76,8 +78,6 @@ export default defineNuxtConfig({
 
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
-
-  ssr: SSR,
 
   app: {
     ...(BASE_DIR
@@ -220,280 +220,16 @@ export default defineNuxtConfig({
   //   // },
   // ],
 
+  // @unimport
   imports: {
-    // @unimport
     // #disable auto-imports; use explicit imports from #imports
     // autoImport: false
 
+    // additional named exports
     dirs: ["./keys"],
-    presets: [
-      {
-        from: "lodash/get",
-        imports: [
-          {
-            name: "default",
-            as: "get",
-          },
-        ],
-      },
-      {
-        from: "lodash/get",
-        imports: [
-          {
-            name: "default",
-            as: "getPath",
-          },
-        ],
-      },
-      {
-        from: "lodash/identity",
-        imports: [{ name: "default", as: "identity" }],
-      },
-      {
-        from: "lodash/isFunction",
-        imports: [{ name: "default", as: "isFunction" }],
-      },
-      {
-        from: "lodash/range",
-        imports: [{ name: "default", as: "range" }],
-      },
-      {
-        from: "lodash/pullAll",
-        imports: [{ name: "default", as: "pullAll" }],
-      },
-      {
-        from: "lodash/size",
-        imports: [{ name: "default", as: "len" }],
-      },
-      {
-        from: "lodash/isEmpty",
-        imports: [{ name: "default", as: "isEmpty" }],
-      },
-      {
-        from: "lodash/first",
-        imports: [{ name: "default", as: "first" }],
-      },
-      {
-        from: "lodash/every",
-        imports: [{ name: "default", as: "every" }],
-      },
-      {
-        from: "lodash/differenceBy",
-        imports: [{ name: "default", as: "differenceBy" }],
-      },
-      {
-        from: "lodash/union",
-        imports: [{ name: "default", as: "union" }],
-      },
-      {
-        from: "lodash/sortBy",
-        imports: [{ name: "default", as: "sortBy" }],
-      },
-      {
-        from: "lodash/once",
-        imports: [{ name: "default", as: "once" }],
-      },
-      {
-        from: "lodash/difference",
-        imports: [{ name: "default", as: "difference" }],
-      },
-      {
-        from: "lodash/sampleSize",
-        imports: [{ name: "default", as: "sampleSize" }],
-      },
-      {
-        from: "lodash/findKey",
-        imports: [{ name: "default", as: "findKey" }],
-      },
-      {
-        from: "lodash/find",
-        imports: [{ name: "default", as: "find" }],
-      },
-      {
-        from: "lodash/unset",
-        imports: [{ name: "default", as: "unset" }],
-      },
-      {
-        from: "lodash/each",
-        imports: [{ name: "default", as: "each" }],
-      },
-      {
-        from: "lodash/cloneDeep",
-        imports: [{ name: "default", as: "cloneDeep" }],
-      },
-      // {
-      //   from: "lodash/clone",
-      //   imports: [{ name: "default", as: "clone" }],
-      // },
-      {
-        from: "lodash/isString",
-        imports: [{ name: "default", as: "isString" }],
-      },
-      {
-        from: "lodash/has",
-        imports: [{ name: "default", as: "has" }],
-      },
-      {
-        from: "lodash/has",
-        imports: [{ name: "default", as: "hasPath" }],
-      },
-      {
-        from: "lodash/pick",
-        imports: [{ name: "default", as: "pick" }],
-      },
-      {
-        from: "lodash/omit",
-        imports: [{ name: "default", as: "omit" }],
-      },
-      {
-        from: "lodash/keys",
-        imports: [{ name: "default", as: "keys" }],
-      },
-      // {
-      //   from: "lodash/isEqual",
-      //   imports: [{ name: "default", as: "isEqual" }],
-      // },
-      {
-        from: "lodash/sample",
-        imports: [{ name: "default", as: "sample" }],
-      },
-      {
-        from: "lodash/take",
-        imports: [{ name: "default", as: "take" }],
-      },
-      {
-        from: "lodash/takeRight",
-        imports: [{ name: "default", as: "takeRight" }],
-      },
-      {
-        from: "lodash/debounce",
-        imports: [{ name: "default", as: "debounce" }],
-      },
-      {
-        from: "lodash/startCase",
-        imports: [{ name: "default", as: "startCase" }],
-      },
-      {
-        from: "lodash/lowerCase",
-        imports: [{ name: "default", as: "lowerCase" }],
-      },
-      {
-        from: "lodash/upperCase",
-        imports: [{ name: "default", as: "upperCase" }],
-      },
-      {
-        from: "lodash/camelCase",
-        imports: [{ name: "default", as: "camelCase" }],
-      },
-      {
-        from: "lodash/kebabCase",
-        imports: [{ name: "default", as: "kebabCase" }],
-      },
-      {
-        from: "lodash/snakeCase",
-        imports: [{ name: "default", as: "snakeCase" }],
-      },
-      {
-        from: "lodash/set",
-        imports: [{ name: "default", as: "set" }],
-      },
-      {
-        from: "lodash/clamp",
-        imports: [{ name: "default", as: "clamp" }],
-      },
-      {
-        from: "lodash/uniqueId",
-        imports: [{ name: "default", as: "uniqueId" }],
-      },
-      {
-        from: "lodash/assign",
-        imports: [{ name: "default", as: "assign" }],
-      },
-      {
-        from: "lodash/some",
-        imports: [{ name: "default", as: "some" }],
-      },
-      {
-        from: "lodash/trimEnd",
-        imports: [{ name: "default", as: "trimEnd" }],
-      },
-      {
-        from: "lodash/trim",
-        imports: [{ name: "default", as: "trim" }],
-      },
-      {
-        from: "lodash/noop",
-        imports: [{ name: "default", as: "noop" }],
-      },
-      {
-        from: "lodash/transform",
-        imports: [{ name: "default", as: "transform" }],
-      },
-      {
-        from: "lodash/map",
-        imports: [{ name: "default", as: "map" }],
-      },
-      {
-        from: "lodash/filter",
-        imports: [{ name: "default", as: "filter" }],
-      },
-      {
-        from: "lodash/reduce",
-        imports: [{ name: "default", as: "reduce" }],
-      },
-      {
-        from: "lodash/escapeRegExp",
-        imports: [{ name: "default", as: "escapeRegExp" }],
-      },
-      {
-        from: "lodash/intersection",
-        imports: [{ name: "default", as: "intersection" }],
-      },
-      {
-        from: "lodash/intersectionBy",
-        imports: [{ name: "default", as: "intersectionBy" }],
-      },
-      {
-        from: "lodash/uniqBy",
-        imports: [{ name: "default", as: "uniqBy" }],
-      },
-      {
-        from: "lodash/uniq",
-        imports: [{ name: "default", as: "uniq" }],
-      },
-      {
-        from: "lodash/merge",
-        imports: [{ name: "default", as: "merge" }],
-      },
-      {
-        from: "lodash/values",
-        imports: [{ name: "default", as: "values" }],
-      },
-      {
-        from: "lodash/last",
-        imports: [{ name: "default", as: "last" }],
-      },
-      {
-        from: "validator/lib/isEmail",
-        imports: [{ name: "default", as: "isEmail" }],
-      },
-      {
-        from: "validator/lib/isMobilePhone",
-        imports: [{ name: "default", as: "isMobilePhone" }],
-      },
-      {
-        from: "validator/lib/isURL",
-        imports: [{ name: "default", as: "isURL" }],
-      },
-      {
-        from: "uuid",
-        imports: [{ name: "v4", as: "uuid" }],
-      },
-      {
-        from: "tree-model",
-        imports: [{ name: "default", as: "TreeModel" }],
-      },
-    ],
+
+    //
+    presets: IMPORTS,
   },
 
   // #https://google-fonts.nuxtjs.org
@@ -573,6 +309,7 @@ export default defineNuxtConfig({
         "fluent:select-all-on-24-regular",
         "game-icons:cardboard-box-closed",
         "ic:round-storefront",
+        "ic:sharp-upload",
         "icon-park-outline:preview-close-one",
         "icon-park-outline:preview-open",
         "ion:ios-cube",
@@ -589,20 +326,31 @@ export default defineNuxtConfig({
         "material-symbols:delete",
         "material-symbols:filter-alt-off-outline",
         "material-symbols:filter-list",
+        "material-symbols:folder-open-outline",
         "material-symbols:group",
         "material-symbols:groups",
+        "material-symbols:label-off-outline-sharp",
+        "material-symbols:label-outline-sharp",
+        "material-symbols:label-sharp",
         "material-symbols:link-rounded",
         "material-symbols:location-on",
         "material-symbols:lock-open-rounded",
         "material-symbols:lock-reset-sharp",
         "material-symbols:mail-outline",
-        "material-symbols:photo-camera",
+        "material-symbols:new-label-outline-sharp",
+        "material-symbols:notifications-active-outline-sharp",
         "material-symbols:photo-camera-rounded",
+        "material-symbols:photo-camera",
         "material-symbols:save",
         "material-symbols:search",
         "material-symbols:sms-outline-rounded",
         "mdi:account-multiple",
+        "mdi:account-settings-variant",
+        "mdi:checkbox-blank-circle-outline",
+        "mdi:checkbox-marked-circle",
         "mdi:dots-vertical",
+        "mdi:eye-off",
+        "mdi:eye",
         "mdi:folder",
         "mdi:information-variant",
         "mdi:key-chain-variant",
@@ -624,18 +372,6 @@ export default defineNuxtConfig({
         "tabler:external-link",
         "tdesign:poweroff",
         "uil:github",
-        "material-symbols:notifications-active-outline-sharp",
-        "material-symbols:label-sharp",
-        "material-symbols:label-outline-sharp",
-        "material-symbols:new-label-outline-sharp",
-        "material-symbols:label-off-outline-sharp",
-        "mdi:checkbox-blank-circle-outline",
-        "mdi:checkbox-marked-circle",
-        "mdi:eye-off",
-        "mdi:eye",
-        "material-symbols:folder-open-outline",
-        "ic:sharp-upload",
-        "mdi:account-settings-variant",
       ],
       //
 
