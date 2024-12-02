00013917060af99adc566e3bc4c9e969a6d3de0f
Vukovic Nikola <admin@nikolav.rs>
2024-12-02 09:25:56 +0100

    Mon, Dec  2, 2024  8:25:56 AM

--- pages/demo/index.vue
+++ pages/demo/index.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
+// ##imports
 import { Demo } from "@/components/dev";
+// ##config:const
+// ##config ##props ##route ##attrs
 definePageMeta({
   layout: "app-default",
   // middleware: "access-debug",
 });
+// ##schemas
+// ##utils
+// ##icons
+// ##refs ##flags ##models
+// ##data ##auth ##state
+// ##computed
+// ##forms ##handlers ##helpers ##small-utils
+// ##watch
+// ##hooks ##lifecycle
+// ##head ##meta
+useHead({ title: "🚧@Demo" });
+// ##provide
+// ##io
 
 // @@eos
 </script>
 <template>
   <section class="page--demo">
-    <Demo />
+    <h1>🚧</h1>
   </section>
 </template>
 <style lang="scss" scoped></style>
+<style module></style>
+<style lang="scss"></style>
